1d9fca621981bcf06ed8d088ce7ffffd71c649b0
Gabriel <gabriel_vieira01@outlook.com>
2022-08-09 12:02:40 -0300

    Entrega Finalizada

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Jhon"
+let sobrenome = "Doyle"
+let anoNascimento = 1991
+let cidade = "Curitiba"
+let idade = 2022 - anoNascimento
+console.log(`Eu sou ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
